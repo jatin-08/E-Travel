d33335850e0ca8557308bf305eafd3cfa68087cf
jatin-08 <yadavjatin137@gmail.com>
2023-02-18 00:02:06 +0530

    intial commit

--- App/signin.php
+++ App/signin.php
@@ -5,24 +5,46 @@ require("connection.php");
 if (isset($_POST['signup'])) {
 
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $name = mysqli_real_escape_string($connection, $_POST['name']);
+    $email = mysqli_real_escape_string($connection, $_POST['email']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+    $conpassword = mysqli_real_escape_string($connection, $_POST['conpassword']);
 
-    $query = "INSERT INTO user_info (name, email, password) VALUES('$name','$email','$password')";
-    $data = mysqli_query($connection, $query);
-    if ($data) {
-        // echo "<script>alert('Information has been stored successfully!')</script>";
+    $pass = password_hash($password, PASSWORD_BCRYPT);
+    $conpass = password_hash($conpassword, PASSWORD_BCRYPT);
 
-        ?>
+    $emialquery = "SELECT * FROM user_info WHERE email = '$email' ";
+    $equery = mysqli_query($connection, $emialquery);
 
-        <meta http-equiv="refresh" content="0; url = http://localhost/E-Travel/App/login-UI.php" />
+    $emailcount = mysqli_num_rows($equery);
 
-        <?php
-
+    if ($emailcount > 0) {
+        echo "email already exists";
     } else {
-        echo "<script>alert('Information has not been stored successfully!')</script>";
+        if ($password === $conpassword) {
+
+            $query = "INSERT INTO user_info (name, email, password, conpassword) VALUES('$name','$email','$pass','$conpass')";
+            $data = mysqli_query($connection, $query);
+
+            if ($data) {
+
+                // echo "<script>alert('Information has been stored successfully!')</script>";
+
+                ?>
+
+                <meta http-equiv="refresh" content="0; url = http://localhost/E-Travel/App/login-UI.php" />
+
+                <?php
+
+            } else {
+                echo "<script>alert('Information has not been stored successfully!')</script>";
+            }
+        } else {
+            echo "password is wrong!";
+        }
     }
+
+
 }
 
 ?>
