930c30e9b20de78769ffbb96187defa9d3a069b5
jatin-08 <yadavjatin137@gmail.com>
2023-04-11 02:36:18 +0530

    done

--- App/reservation.php
+++ App/reservation.php
@@ -18,8 +18,21 @@ require_once('header.php');
 <body class="bg-light">
 
     <!-- Header Section Start -->
-    <section class="container-fluid bg-image">
-        <img src="../Public/Assests/image/airplane.jpg" class="image-fluid" alt="image">
+    <section class="container-fluid header-bg-image bg-light py-2">
+        <div class="row px-5 py-2">
+            <div class="col-lg-6 col-12 pt-4">
+                <div class="card border-0" style="width: 100%; background: none;">
+                    <div class="card-body">
+                        <h1 class="card-title fw-600 ubuntu py-3">Find Adventure awaits<br>Book your escape
+                        </h1>
+                        <a href="#" class="card-link header-bg-btn fw-bold poppins">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 order-lg-last order-first col-12">
+                <img src="../Public\Assests\image\bg-airplane.jpg" class="w-100" alt="image">
+            </div>
+        </div>
     </section>
     <!-- Header Section End -->
 
@@ -27,30 +40,89 @@ require_once('header.php');
 
     <section class="container-fluid my-5">
         <div class="conatiner">
-            <div class="row px-5">
+            <div class="row m-5">
                 <ul class="list-unstyled d-flex">
                     <li class="box">
                         <a href="#" class="text-decoration-none shadow-sm">All Offers</a>
                     </li>
                     <li class="box">
-                        <a href="#" class="text-decoration-none shadow-sm"><i class="fa-solid fa-plane-departure px-2"></i>Flight</a>
+                        <a href="#" class="text-decoration-none shadow-sm"><i
+                                class="fa-solid fa-plane-departure px-2"></i>Flight</a>
                     </li>
                     <li class="box">
-                        <a href="#" class="text-decoration-none shadow-sm"><i class="fa-solid fa-train px-2"></i>Railway</a>
+                        <a href="#" class="text-decoration-none shadow-sm"><i
+                                class="fa-solid fa-train px-2"></i>Railway</a>
                     </li>
                     <li class="box">
                         <a href="#" class="text-decoration-none shadow-sm"><i class="fa-solid fa-bus px-2"></i>Bus</a>
                     </li>
                 </ul>
             </div>
-            <div class="row p-5">
-                <div class="card" style="width: 25rem;">
-                  <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <strong class="card-title fs-5">Grab Now: Flat 12% OFF* on Domestic Flight</strong>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-success">Book Now</a>
-                  </div>
+            <div class="island mx-5">
+                <div class="island-box rounded card border-0 shadow">
+                    <div class="image-box rounded">
+                        <img src="../Public/Assests\image\flight1.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <strong class="poppins card-text px-3 pt-2 fs-5">Grap Now: Flat 12% OFF* on Domestic Flight</strong>
+                    <p class="poppins card-text px-3 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Maxime, reiciendis harum animi nihil
+                        aperiam excepturi ut esse a voluptates inventore.</p>
+                    <div class="explore-btn d-flex justify-content-between">
+                        <p class="text-decoration-underline text-dark px-3 pt-2">T&C Apply</p>
+                        <a href="#" class="text-decoration-none text-white mb-3 mx-2">Book Now</a>
+                    </div>
+                </div>
+                <div class="island-box rounded card border-0 shadow">
+                    <div class="image-box rounded">
+                        <img src="../Public/Assests\image\flight1.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <strong class="poppins card-text px-3 pt-2 fs-5">Grap Now: Flat 12% OFF* on Domestic Flight</strong>
+                    <p class="poppins card-text px-3 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Maxime, reiciendis harum animi nihil
+                        aperiam excepturi ut esse a voluptates inventore.</p>
+                    <div class="explore-btn d-flex justify-content-between">
+                        <p class="text-decoration-underline text-dark px-3 pt-2">T&C Apply</p>
+                        <a href="#" class="text-decoration-none text-white mb-3 mx-2">Book Now</a>
+                    </div>
+                </div>
+                <div class="island-box rounded card border-0 shadow">
+                    <div class="image-box rounded">
+                        <img src="../Public/Assests\image\flight1.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <strong class="poppins card-text px-3 pt-2 fs-5">Grap Now: Flat 12% OFF* on Domestic Flight</strong>
+                    <p class="poppins card-text px-3 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Maxime, reiciendis harum animi nihil
+                        aperiam excepturi ut esse a voluptates inventore.</p>
+                    <div class="explore-btn d-flex justify-content-between">
+                        <p class="text-decoration-underline text-dark px-3 pt-2">T&C Apply</p>
+                        <a href="#" class="text-decoration-none text-white mb-3 mx-2">Book Now</a>
+                    </div>
+                </div>
+                <div class="island-box rounded card border-0 shadow">
+                    <div class="image-box rounded">
+                        <img src="../Public/Assests\image\flight1.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <strong class="poppins card-text px-3 pt-2 fs-5">Grap Now: Flat 12% OFF* on Domestic Flight</strong>
+                    <p class="poppins card-text px-3 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Maxime, reiciendis harum animi nihil
+                        aperiam excepturi ut esse a voluptates inventore.</p>
+                    <div class="explore-btn d-flex justify-content-between">
+                        <p class="text-decoration-underline text-dark px-3 pt-2">T&C Apply</p>
+                        <a href="#" class="text-decoration-none text-white mb-3 mx-2">Book Now</a>
+                    </div>
+                </div>
+                <div class="island-box rounded card border-0 shadow">
+                    <div class="image-box rounded">
+                        <img src="../Public/Assests\image\flight1.jpg" class="rounded-top img-fluid" alt="image">
+                    </div>
+                    <strong class="poppins card-text px-3 pt-2 fs-5">Grap Now: Flat 12% OFF* on Domestic Flight</strong>
+                    <p class="poppins card-text px-3 pt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Maxime, reiciendis harum animi nihil
+                        aperiam excepturi ut esse a voluptates inventore.</p>
+                    <div class="explore-btn d-flex justify-content-between">
+                        <p class="text-decoration-underline text-dark px-3 pt-2">T&C Apply</p>
+                        <a href="#" class="text-decoration-none text-white mb-3 mx-2">Book Now</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -58,12 +130,30 @@ require_once('header.php');
 
     <!-- Main Section End -->
 
+    <!-- Query Section Start -->
 
-<?php
+    <div class="container-fluid my-5">
+        <div class="row card border-0 shadow mx-5 g-3">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Enter Your Name">
+            </div>
+            <div class="col">
+                <input type="email" class="form-control" placeholder="Enter Your E-mail">
+            </div>
+            <div class="col">
+                <a href="#" class="header-bg-btn mb-3 mt-2">Submit</a>
+            </div>
+        </div>
+    </div>
 
-require_once("../App/footer.php");
+    <!-- Query Section End -->
 
-?>
+
+    <?php
+
+    require_once("../App/footer.php");
+
+    ?>
 
 </body>
 
